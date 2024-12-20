f6a54028f24c00cb620e026ab7c2c9f10c4c57cd
Abdullah Sadaqa <eng.abdullahhasan2016@gmail.com>
2024-12-20 17:36:54 +0200

    feature(invoices): add routes for deleting, editing, and marking invoices as paid

---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,37 @@
+import { Suspense } from "react";
+import Link from "next/link";
+import { PlusIcon } from "lucide-react";
+
+import { InvoiceList } from "@/components/invoice-list/InvoiceList";
+import { buttonVariants } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Skeleton } from "@/components/ui/skeleton";
+
+export default function InvoicesRoute() {
+  return (
+    <Card>
+      <CardHeader>
+        <div className="flex items-center justify-between">
+          <div>
+            <CardTitle className="text-2xl font-bold">Invoices</CardTitle>
+            <CardDescription>Manage your invoices right here</CardDescription>
+          </div>
+          <Link href="/dashboard/invoices/create" className={buttonVariants()}>
+            <PlusIcon /> Create Invoice
+          </Link>
+        </div>
+      </CardHeader>
+      <CardContent>
+        <Suspense fallback={<Skeleton className="w-full h-[500px]" />}>
+          <InvoiceList />
+        </Suspense>
+      </CardContent>
+    </Card>
+  );
+}
