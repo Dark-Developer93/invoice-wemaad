4727e6db377adf751854ce7630dc502f836ddf83
Abdullah Sadaqa <eng.abdullahhasan2016@gmail.com>
2024-12-21 19:34:50 +0200

    feat(types): add TypeScript types for invoice and contact form email properties

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,1 +1,22 @@
 export type Currency = "USD" | "EUR" | "EGP";
+
+interface InvoiceEmailProps {
+  clientName: string;
+  invoiceNumber: string;
+  invoiceDueDate: string;
+  invoiceAmount: string;
+  invoiceLink: string;
+}
+
+export type NewInvoiceEmailProps = InvoiceEmailProps;
+
+export type UpdatedInvoiceEmailProps = InvoiceEmailProps;
+
+export type ReminderInvoiceEmailProps = InvoiceEmailProps;
+
+export type ContactFormEmailProps = {
+  firstName: string;
+  lastName: string;
+  email: string;
+  message: string;
+};
